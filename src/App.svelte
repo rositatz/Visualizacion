801e73b6c701952b12df886309289a53787e63e7
rositatz <rosita61tz@hotmail.com>
2025-05-24 23:17:02 -0300

    fondo

--- src/App.svelte
+++ src/App.svelte
@@ -54,134 +54,135 @@
   <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
 </head>
 
-
-<div class="explicacion-reproductor">
-  <h2>Cuando el ritmo se vuelve interfaz</h2>
-  <p>
-    Este recorrido muestra cómo la música y la tecnología han transformado nuestra forma de escuchar desde los años 80 hasta 2020. 
-    A través de canciones representativas de cada década, se analizan datos como la cantidad de reproducciones, su posición en el 
-    Billboard Top 100, el género y el nivel de danceability. Además, se observa cómo los formatos han cambiado, pasando de vinilos 
-    y casetes a reproductores digitales y plataformas de streaming, reflejando la evolución en la forma en que elegimos y disfrutamos la música.
-    Así, cada época no solo define un sonido, sino también la manera en que la música se convierte en parte de nuestra vida y memoria.
-  </p>
-  <br>
-  <br>
-  <h3>Canciones que marcaron épocas</h3>
-</div>
-
 <body>
-
-  <!-- Círculos de colores de los géneros -->
-<div class="fila-codificacion fila-superior">
-  <div class="codificacion-wrapper">
-    <h3 class="titulo-codificacion">Género</h3>
-    <div class="generos-circulos">
-    <div class="genero">
-      <div class="circulo" style="background-color: #00CC66;"></div>
-      <span>Pop</span>
-    </div>
-    <div class="genero">
-      <div class="circulo" style="background-color: #CC0000;"></div>
-      <span>Rock</span>
-    </div>
-    <div class="genero">
-      <div class="circulo" style="background-color: #CC0066;"></div>
-      <span>Indie</span>
-    </div>
-    <div class="genero">
-      <div class="circulo" style="background-color: #001BCC;"></div>
-      <span>Electrónica</span>
-    </div>
-    <div class="genero">
-      <div class="circulo" style="background-color: #CCB400;"></div>
-      <span>Reguetón</span>
-    </div>
-    <div class="genero">
-      <div class="circulo" style="background-color: #000000;"></div>
-      <span >Rap</span>
-    </div>
-  </div>
-  </div>
+  <div class="explicacion-reproductor">
+    <h2>Cuando el ritmo se vuelve interfaz</h2>
+    <p>
+      Este recorrido muestra cómo la música y la tecnología han transformado nuestra forma de escuchar desde los años 80 hasta 2020. 
+      A través de canciones representativas de cada década, se analizan datos como la cantidad de reproducciones, su posición en el 
+      Billboard Top 100, el género y el nivel de danceability. Además, se observa cómo los formatos han cambiado, pasando de vinilos 
+      y casetes a reproductores digitales y plataformas de streaming, reflejando la evolución en la forma en que elegimos y disfrutamos la música.
+      Así, cada época no solo define un sonido, sino también la manera en que la música se convierte en parte de nuestra vida y memoria.
+    </p>
+    <br>
+    <br>
+    <h3>Canciones que marcaron épocas</h3>
   </div>
 
-  <div class="fila-codificacion">
-  <div class="codificacion-wrapper">
-    <h3 class="titulo-codificacion">Popularidad</h3>
-    <div class="popularidad-circulos">
-      <div class="circulo pequeno"></div>
-      <div class="circulo mediano"></div>
-      <div class="circulo grande"></div>
-    </div>
-  </div>
 
-  <div class="codificacion-wrapper billboard-wrapper">
-    <h3 class="titulo-codificacion">Ranking Billboard</h3>
-    <div class="billboard-numero">77</div>
-  </div>
-  </div>
-
-<div class="fila-codificacion">
-  <div class="item-codificacion">
-    <div class="titulo-codificacion">Quién eligió la canción</div>
-    <div style="display: flex; gap: 25px; margin-bottom: 10px;">
-        <div style="text-align: center;">
-          <img src="/images/Var.png" alt ="Var" style="height: 25px;" /><br>Var
-        </div>
-        <div style="text-align: center;">
-          <img src="/images/Steffy.png" alt ="Steffy" style="height: 25px;" /><br>Steffy
-        </div>
-        <div style="text-align: center;">
-          <img src="/images/Rosita.png" alt ="Rosita" style="height: 25px;" /><br>Rosita
+  <div class="codificaciones">
+    <!-- Círculos de colores de los géneros -->
+    <div class="fila-codificacion fila-superior">
+      <div class="codificacion-wrapper">
+        <h3 class="titulo-codificacion">Género</h3>
+        <div class="generos-circulos">
+          <div class="genero">
+            <div class="circulo" style="background-color: #00CC66;"></div>
+            <span>Pop</span>
+          </div>
+          <div class="genero">
+            <div class="circulo" style="background-color: #CC0000;"></div>
+            <span>Rock</span>
+          </div>
+          <div class="genero">
+            <div class="circulo" style="background-color: #CC0066;"></div>
+            <span>Indie</span>
+          </div>
+          <div class="genero">
+            <div class="circulo" style="background-color: #001BCC;"></div>
+            <span>Electrónica</span>
+          </div>
+          <div class="genero">
+            <div class="circulo" style="background-color: #CCB400;"></div>
+            <span>Reguetón</span>
+          </div>
+          <div class="genero">
+            <div class="circulo" style="background-color: #000000;"></div>
+            <span >Rap</span>
+          </div>
         </div>
       </div>
-  </div>
-
-  <div class="item-codificacion">
-    <div class="titulo-codificacion">Danceability</div>
-    <div class="ondas-danceability">
-      <svg viewBox="0 0 100 40" class="onda onda-baja">
-        <path d="M 0 20 C 10 10, 20 30, 30 20 C 40 10, 50 30, 60 20 C 70 10, 80 30, 90 20, 100 20" />
-      </svg>
-
-      <svg viewBox="0 0 100 40" class="onda onda-media">
-        <path d="M 0 20 C 10 0, 20 40, 30 20 C 40 0, 50 40, 60 20 C 70 0, 80 40, 90 20, 100 20" />
-      </svg>
-
-      <svg viewBox="0 0 100 40" class="onda onda-alta">
-        <path d="M 0 20 C 10 -10, 20 50, 30 20 C 40 -10, 50 50, 60 20 C 70 -10, 80 50, 90 20, 100 20" />
-      </svg>
     </div>
-</div>
+
+      <div class="fila-codificacion">
+      <div class="codificacion-wrapper">
+        <h3 class="titulo-codificacion">Popularidad</h3>
+        <div class="popularidad-circulos">
+          <div class="circulo pequeno"></div>
+          <div class="circulo mediano"></div>
+          <div class="circulo grande"></div>
+        </div>
+      </div>
+
+      <div class="codificacion-wrapper billboard-wrapper">
+        <h3 class="titulo-codificacion">Ranking Billboard</h3>
+        <div class="billboard-numero">77</div>
+      </div>
+      </div>
+
+    <div class="fila-codificacion">
+      <div class="item-codificacion">
+        <div class="titulo-codificacion">Quién eligió la canción</div>
+        <div style="display: flex; gap: 25px; margin-bottom: 10px;">
+            <div style="text-align: center;">
+              <img src="/images/Var.png" alt ="Var" style="height: 25px;" /><br>Var
+            </div>
+            <div style="text-align: center;">
+              <img src="/images/Steffy.png" alt ="Steffy" style="height: 25px;" /><br>Steffy
+            </div>
+            <div style="text-align: center;">
+              <img src="/images/Rosita.png" alt ="Rosita" style="height: 25px;" /><br>Rosita
+            </div>
+          </div>
+      </div>
+
+      <div class="item-codificacion">
+        <div class="titulo-codificacion">Danceability</div>
+        <div class="ondas-danceability">
+          <svg viewBox="0 0 100 40" class="onda onda-baja">
+            <path d="M 0 20 C 10 10, 20 30, 30 20 C 40 10, 50 30, 60 20 C 70 10, 80 30, 90 20, 100 20" />
+          </svg>
+
+          <svg viewBox="0 0 100 40" class="onda onda-media">
+            <path d="M 0 20 C 10 0, 20 40, 30 20 C 40 0, 50 40, 60 20 C 70 0, 80 40, 90 20, 100 20" />
+          </svg>
+
+          <svg viewBox="0 0 100 40" class="onda onda-alta">
+            <path d="M 0 20 C 10 -10, 20 50, 30 20 C 40 -10, 50 50, 60 20 C 70 -10, 80 50, 90 20, 100 20" />
+          </svg>
+        </div>
+    </div>
+  </div>
+  </div>  
+
+    {#each decadas as decada}
+      <CancionesPorDecada
+        {decada}
+        canciones={cancionesPorDecada[decada]}
+        {colorGenero}
+        {obtenerDiametro}
+        {iconosPlayPause}
+        {simboloSelector}
+      />
+    {/each}
+
+    <p>
+      Cada canción se transforma en un círculo que late con su propia energía, mostrando su popularidad y estilo en un universo visual que invita a descubrir música con solo mirar.
+
+      <br>
+      <strong>El tamaño</strong> del círculo representa su popularidad: cuantas más reproducciones tiene una canción, más grande se vuelve, ocupando su lugar en el escenario visual.
+      <br>
+      <strong>El color</strong> refleja el género musical, pintando la visualización con una paleta tan diversa como la música misma.
+    </p>
 
 
-  {#each decadas as decada}
-    <CancionesPorDecada
-      {decada}
-      canciones={cancionesPorDecada[decada]}
-      {colorGenero}
-      {obtenerDiametro}
-      {iconosPlayPause}
-      {simboloSelector}
-    />
-  {/each}
-
-  <p>
-    Cada canción se transforma en un círculo que late con su propia energía, mostrando su popularidad y estilo en un universo visual que invita a descubrir música con solo mirar.
-
-    <br>
-    <strong>El tamaño</strong> del círculo representa su popularidad: cuantas más reproducciones tiene una canción, más grande se vuelve, ocupando su lugar en el escenario visual.
-    <br>
-    <strong>El color</strong> refleja el género musical, pintando la visualización con una paleta tan diversa como la música misma.
-  </p>
-
-
-  <p>
-    Si una canción llegó al <strong>Billboard</strong>, su número aparece en el centro del círculo, como un pequeño trofeo de prestigio.
-    <br>
-    Cada selector tiene su símbolo: <strong>Var</strong> es un triángulo, como el clásico botón de play <strong>Steffy</strong> un corazón, porque siente cada canción y <strong>Rosita</strong> una estrella porque brilla con su gusto musical, porque cada quien elige con su estilo.
-    <br>
-    Finalmente, la <strong>danceability</strong> se traduce en la forma y movimiento, como si cada reproductor tuviera su propio pulso, latiendo al ritmo de su melodía.
-    <br>
-    En el centro de cada canción, <strong>una onda estática</strong> muestra el ritmo de la música, adoptando el color de su género musical. La amplitud de la onda refleja la danceability de la canción: cuanto más bailable, más pronunciada será la onda.
-  </p>
+    <p>
+      Si una canción llegó al <strong>Billboard</strong>, su número aparece en el centro del círculo, como un pequeño trofeo de prestigio.
+      <br>
+      Cada selector tiene su símbolo: <strong>Var</strong> es un triángulo, como el clásico botón de play <strong>Steffy</strong> un corazón, porque siente cada canción y <strong>Rosita</strong> una estrella porque brilla con su gusto musical, porque cada quien elige con su estilo.
+      <br>
+      Finalmente, la <strong>danceability</strong> se traduce en la forma y movimiento, como si cada reproductor tuviera su propio pulso, latiendo al ritmo de su melodía.
+      <br>
+      En el centro de cada canción, <strong>una onda estática</strong> muestra el ritmo de la música, adoptando el color de su género musical. La amplitud de la onda refleja la danceability de la canción: cuanto más bailable, más pronunciada será la onda.
+    </p>
 </body>
